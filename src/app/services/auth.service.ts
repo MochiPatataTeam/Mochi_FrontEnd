a23a63b7b4092b0189da8ba99edfc585e5d67bb5
Sara <sprietourban@safareyes.es>
2024-01-26 20:30:56 +0100

    Sara - Comentarios en el frontend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,12 +6,19 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class AuthService {
-  private url= 'http://127.0.0.1:8000/api/usuario';
-
+  private lista_usuarios= 'http://127.0.0.1:8000/api/usuario';
+  private lista_comentarios ='http://127.0.0.1:8000/api/comentario'
+  private lista_respuestas ='http://127.0.0.1:8000/api/respuesta'
   usuario(): Observable<any>{
-    return this.http.get(`${this.url}`);
+    return this.http.get(`${this.lista_usuarios}`);
+  }
+  comentario(): Observable<any>{
+    return this.http.get(`${this.lista_comentarios}`)
   }
 
+  respuesta():Observable<any>{
+    return this.http.get(`${this.lista_respuestas}`)
+  }
 
   constructor(private http: HttpClient) {
 
--- src/app/ventanas/ventana-reproduccion/ventana-reproduccion.component.ts
+++ src/app/ventanas/ventana-reproduccion/ventana-reproduccion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-ventana-reproduccion',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./ventana-reproduccion.component.css']
 })
 export class VentanaReproduccionComponent {
+  comentarios: any[]=[];
+  respuestas: any[]=[];
+
+  constructor(private authservice: AuthService) {
+  }
+
+  //Listar comentarios
+  ngOnInit():void{
+    this.authservice.comentario().subscribe(
+      (response)=>{
+        console.log(response)
+        this.comentarios=response;
+      },
+      (error)=>{console.log(error)}
+    )
+  }
 
 }
