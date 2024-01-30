f5ac300aedb9c9bf4bfd6c6549d082ffa067ba2c
rksl64 <iferreirafernandez@safareyes.es>
2024-01-30 20:56:24 +0100

    Ismael - subo arreglo y redireccion de video medio lista

--- src/app/ventanas/ventana-inicio/ventana-inicio.component.ts
+++ src/app/ventanas/ventana-inicio/ventana-inicio.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { AuthService} from "../../services/auth.service";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-ventana-inicio',
@@ -9,9 +10,8 @@ import { AuthService} from "../../services/auth.service";
 export class VentanaInicioComponent {
 
   patata: any[]=[];
-
-  constructor(private authservice:AuthService){
-
+  video: any[]=[];
+  constructor(private authservice:AuthService, private sanitizer: DomSanitizer){
   }
   ngOnInit():void{
     this.authservice.usuario().subscribe(
@@ -23,6 +23,27 @@ export class VentanaInicioComponent {
 (error)=>{console.log(error)}
     )
 
+    this.authservice.video().subscribe(
+      (response)=>{
+        this.video=response;
+        this.sanitizarUrls();
+      },
+
+      (error)=>{console.log(error)}
+    )
 
   }
+
+  sanitizarUrls() {
+    for (const vi of this.video) {
+      // Asegúrate de que 'url' sea una propiedad válida de tu objeto de video
+      vi.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(vi.url);
+    }
+  }
+
+  redirigirAotraPagina() {
+    window.location.href = 'http://localhost:4200/reproducir';
+  }
+
+
 }
