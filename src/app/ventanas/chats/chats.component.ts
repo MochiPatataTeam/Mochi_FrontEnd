d9d3cbf027c43d2c76e8bcefd8b8f3f2a14772b2
Antonio López <tonidiez17@gmail.com>
2024-01-30 21:00:15 +0100

    Merge branch 'Dev' of https://github.com/ToniDiez/Mochi_FrontEnd into Dev

--- src/app/ventanas/chats/chats.component.ts
+++ src/app/ventanas/chats/chats.component.ts
@@ -1,12 +1,12 @@
 import {
   Component,
-  ChangeDetectorRef,
   ChangeDetectionStrategy,
   OnInit,
+  TemplateRef,
 } from '@angular/core';
-import { Persona } from 'src/app/componentes/Interfaces/Persona';
-import { Chat } from 'src/app/componentes/Interfaces/Chat';
-import { Mensajes } from 'src/app/componentes/Interfaces/Mensajes';
+import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
+import { NgIfContext } from '@angular/common';
 
 @Component({
   selector: 'app-chats',
@@ -15,199 +15,96 @@ import { Mensajes } from 'src/app/componentes/Interfaces/Mensajes';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatsComponent implements OnInit {
-  persona: Persona = {
-    1: {
-      id: 1,
-      nombre: 'Manuel',
-      img: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-    },
-    2: {
-      id: 2,
-      nombre: 'Federico',
-      img: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-    },
-    3: {
-      id: 3,
-      nombre: 'Federicaa',
-      img: 'https://bootdey.com/img/Content/avatar/avatar8.png',
-    },
-  };
+  mensajes: any;
 
-  chat: Chat = {
-    1: {
-      id: 1,
-      id_usuario: 1,
-      id_usuario2: 2,
-      arrayMensajes: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    },
-    2: { id: 2, id_usuario: 1, id_usuario2: 3, arrayMensajes: [10, 11] },
-  };
+  constructor(private authservice: AuthService) {}
 
-  mensajes: Mensajes = {
-    1: {
-      id: 1,
-      mensaje: 'Hola..',
-      fecha: '10-15-2024',
-      id_usuario: 1,
-    },
-    2: {
-      id: 2,
-      mensaje: 'Holiiiiiiiiiiiiiiiiiiiiiii',
-      fecha: '13-15-2024',
-      id_usuario: 2,
-    },
-    3: {
-      id: 3,
-      mensaje: '¿Qué tal?',
-      fecha: '19-15-2024',
-      id_usuario: 1,
-    },
-    4: {
-      id: 4,
-      mensaje: 'Deseando irme a mi casa',
-      fecha: '13-15-2024',
-      id_usuario: 2,
-    },
-    5: {
-      id: 5,
-      mensaje: 'y tu?',
-      fecha: '13-15-2024',
-      id_usuario: 2,
-    },
-    6: {
-      id: 6,
-      mensaje: 'Igual xd',
-      fecha: '19-15-2024',
-      id_usuario: 1,
-    },
-    7: {
-      id: 7,
-      mensaje: 'Quedamos mañana por lo de ayer?',
-      fecha: '19-15-2024',
-      id_usuario: 1,
-    },
-    8: {
-      id: 8,
-      mensaje: 'En el bar de la esquina de mi casa?',
-      fecha: '19-15-2024',
-      id_usuario: 1,
-    },
-    9: {
-      id: 9,
-      mensaje: 'Hola..',
-      fecha: '10-15-2024',
-      id_usuario: 2,
-    },
-    10: {
-      id: 10,
-      mensaje: 'Hola..',
-      fecha: '10-15-2024',
-      id_usuario: 1,
-    },
-    11: {
-      id: 11,
-      mensaje: 'Holiiiiiiiiii',
-      fecha: '10-15-2024',
-      id_usuario: 3,
-    },
-  };
-
-  idChatBuscado!: number;
-  idPersona: number = 1;
-  chatsRelacionados: any[] = [];
+  id: number = 1;
+  mensajesConver: any[] = [];
+  contactos: any[] = [];
+  contactos$ = this.authservice.contactos(this.id);
   nuevoMensaje: string = '';
   chatSeleccionado: any = null;
-
+  mensajes$!: Observable<any[]>;
+  loading!: TemplateRef<NgIfContext<any>> | null;
+  mensajesJuntos: any[] = [];
+  fechaFormateada!: string;
+  idPersona!: number;
+  nombreIdPersona!: string;
   ngOnInit(): void {
-    this.buscarChatsRelacionados();
-    //metodo de traer los mensajes de base de datos
-  }
-
-  buscarChatsRelacionados() {
-    this.chatsRelacionados = this.buscarChatsPorPersona(this.idPersona);
-  }
-
-  seleccionarChat(chat: any) {
-    this.chatSeleccionado = chat;
-  }
-
-  buscarChatsPorPersona(idPersona: number): any[] {
-    return Object.values(this.chat)
-      .filter(
-        (c: any) => c.id_usuario === idPersona || c.id_usuario2 === idPersona
-      )
-      .map((c: any) => ({
-        id: c.id,
-        id_usuario: c.id_usuario,
-        id_usuario2: c.id_usuario2,
-        arrayMensajes: c.arrayMensajes,
-      }));
-  }
-
-  obtenerNombrePersona(idPersona: number): string {
-    return this.persona[idPersona].nombre;
-  }
-
-  obtenerNombre(idPersona: number): string {
-    return this.obtenerNombrePersona(idPersona);
-  }
-
-  enviarMensaje() {
-    if (this.nuevoMensaje.trim() === '') {
-      return;
-    }
-
-    const nuevoMensaje = {
-      id: this.generarNuevoIdMensaje(),
-      mensaje: this.nuevoMensaje,
-      fecha: this.obtenerFechaActual(),
-      id_usuario: this.idPersona,
-    };
-
-    const chatSeleccionadoOriginal = this.chatSeleccionado;
-
-    chatSeleccionadoOriginal.arrayMensajes.push(nuevoMensaje.id);
-
-    this.mensajes[nuevoMensaje.id] = nuevoMensaje;
-
-    const mensajesChatSeleccionado = chatSeleccionadoOriginal.arrayMensajes.map(
-      (mensajeId: number) => this.mensajes[mensajeId]
+    this.authservice.contactos(this.id).subscribe(
+      (response) => {
+        console.log('aaaaaaaaa', response);
+        this.contactos = response;
+      },
+      (error) => {
+        console.log(error);
+      }
     );
+  }
+  mensajesChat(idPersona: number, nombre: string) {
+    this.nombreIdPersona = nombre;
+    this.idPersona = idPersona;
 
-    console.log('Chat Seleccionado:', chatSeleccionadoOriginal);
-    console.log('Mensajes del Chat Seleccionado:', mensajesChatSeleccionado);
-
-    this.chatSeleccionado = chatSeleccionadoOriginal;
-
-    this.datos(
-      chatSeleccionadoOriginal.id,
-      chatSeleccionadoOriginal.id_usuario,
-      chatSeleccionadoOriginal.id_usuario2,
-      chatSeleccionadoOriginal.arrayMensajes
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaa', this.idPersona);
+    this.mensajes$ = this.authservice.mensajes(this.id, idPersona);
+    this.mensajes$.subscribe(
+      (response) => {
+        this.mensajesConver = response;
+        this.chatSeleccionado = 1;
+        this.imprimirMensajesPorEmisor(this.mensajesConver);
+        this.nombreIdPersona = nombre;
+      },
+      (error) => {
+        console.log(error);
+      }
     );
+  }
 
+  imprimirMensajesPorEmisor(mensajesConver: any[]) {
+    const mensajesEmisor: any[] = [];
+    const mensajesReceptor: any[] = [];
+
+    mensajesConver.forEach((mensaje) => {
+      if (mensaje.idEmisor === this.id.toString()) {
+        mensaje.emisor = true;
+        mensajesEmisor.push(mensaje);
+      } else {
+        mensaje.emisor = false;
+        mensajesReceptor.push(mensaje);
+      }
+    });
+
+    const mensajesUnidos = [...mensajesEmisor, ...mensajesReceptor];
+
+    mensajesUnidos.sort((a, b) => a.id - b.id);
+
+    console.log('Mensajes', mensajesUnidos);
+    this.mensajesJuntos = mensajesUnidos;
+  }
+  enviarMensaje(mensaje: string, idPersona: number) {
+    const fechaActual = new Date();
+    this.fechaFormateada = `${fechaActual
+      .getDate()
+      .toString()
+      .padStart(2, '0')}/${(fechaActual.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}/${fechaActual.getFullYear()}`;
+
+    console.log(mensaje);
+    this.authservice
+      .enviarMensaje(mensaje, this.fechaFormateada, this.id, idPersona)
+      .subscribe(
+        (response) => {
+          console.log('aaaaaaaaa', response);
+          this.contactos = response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    this.mensajesChat(idPersona, this.nombreIdPersona);
     this.nuevoMensaje = '';
-  }
-  datos(
-    id: number,
-    id_usuario: number,
-    id_usuario2: number,
-    arrayMensajes: number[]
-  ) {
-    const Chat = {
-      id: id,
-      id_usuario: id_usuario,
-      id_usuario2: id_usuario2,
-      arrayMensajes: arrayMensajes,
-    };
-    console.log(Chat);
-  }
-  private generarNuevoIdMensaje(): number {
-    return Object.keys(this.mensajes).length + 1;
-  }
-
-  private obtenerFechaActual(): string {
-    const fecha = new Date();
-    return `${fecha.getMonth() + 1}-${fecha.getDate()}-${fecha.getFullYear()}`;
   }
 }
