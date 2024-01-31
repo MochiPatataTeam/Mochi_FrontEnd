20a3175d9e9fad4d30ea789226a204daea9c3db2
Antonio López <tonidiez17@gmail.com>
2024-01-31 17:28:48 +0100

    cambio de registro

--- src/app/ventanas/registro/registro.component.ts
+++ src/app/ventanas/registro/registro.component.ts
@@ -10,8 +10,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class RegistroComponent {
   steps: string[] = [
     'Datos Personales',
-    'Datos Del Canal',
-    'Preguntas Frecuentes',
+    'Datos Del Canal'
   ];
   constructor(private sanitizer: DomSanitizer) {}
 
@@ -27,128 +26,6 @@ export class RegistroComponent {
   descripcion!: string;
   suscripciones!: 0;
   imagen!: string;
-  respuesta!: string;
-  respuesta2!: string;
-  respuesta3!: string;
-  respuesta4!: string;
-
-  preguntas = {
-    respuesta: this.respuesta,
-    respuesta2: this.respuesta2,
-    respuesta3: this.respuesta3,
-    respuesta4: this.respuesta4,
-  };
-
-  inputsStep1 = {
-    nombre: { type: 'text', label: 'Nombre', name: 'nombre' },
-    primerApellido: {
-      type: 'text',
-      label: 'Primer Apellido',
-      name: 'primerApellido',
-    },
-    segundoApellido: {
-      type: 'text',
-      label: 'Segundo Apellido',
-      name: 'segundoApellido',
-    },
-    telefono: { type: 'text', label: 'Telefono', name: 'telefono' },
-    email: { type: 'email', label: 'Email', name: 'email' },
-  };
-  inputsStep2 = {
-    canal: { type: 'text', label: 'Nombre del Canal', name: 'nombre_canal' },
-    username: { type: 'text', label: 'Usuario', name: 'username' },
-    password: { type: 'password', label: 'Contraseña', name: 'password' },
-    descripcion: {
-      type: 'textarea',
-      label: 'Descripcion',
-      name: 'descripcion',
-    },
-    imagen: { type: 'text', label: 'Imagen', name: 'imagen' },
-  };
-  inputsStep3 = {
-    pregunta1: { type: 'text', label: 'Pregunta 1', name: 'respuesta' },
-    pregunta2: { type: 'text', label: 'Pregunta 2', name: 'respuesta2' },
-    pregunta3: { type: 'text', label: 'Pregunta 3', name: 'respuesta3' },
-    pregunta4: { type: 'text', label: 'Pregunta 4', name: 'respuesta4' },
-  };
-
-  generarInputs(inputs: any) {
-    let htmlString = '';
-
-    for (const key in inputs) {
-      if (inputs.hasOwnProperty(key)) {
-        const inputType = inputs[key].type;
-        const inputLabel = inputs[key].label;
-        const inputName = inputs[key].name;
-        htmlString += this.generarInput(key, inputType, inputLabel, inputName);
-      }
-    }
-
-    return this.sanitizer.bypassSecurityTrustHtml(htmlString);
-  }
-
-  generarInput(
-    nombre: string,
-    tipo: string,
-    label: string,
-    inputName: string
-  ): string {
-    switch (tipo) {
-      case 'text':
-        return `
-          <div class="input-box">
-            <input
-              required=""
-              name="${inputName}"
-              type="text"
-              class="input"
-              [(ngModel)]="${nombre}"
-            />
-            <label class="label text--flicking">${label}:</label>
-          </div>
-        `;
-      case 'password':
-        return `
-          <div class="input-box">
-            <input
-              required=""
-              name="${inputName}"
-              type="password"
-              class="input"
-              [(ngModel)]="${nombre}"
-            />
-            <label class="label text--flicking">${label}:</label>
-          </div>
-        `;
-      case 'textarea':
-        return `
-          <div class="input-box">
-            <textarea
-              required=""
-              name="${inputName}"
-              class="textarea"
-              [(ngModel)]="${nombre}"
-            ></textarea>
-            <label class="label text--flicking">${label}:</label>
-          </div>
-        `;
-      case 'email':
-        return `
-          <div class="input-box">
-            <input
-              required=""
-              name="${inputName}"
-              type="email"
-              class="input"
-              [(ngModel)]="${nombre}"
-            />
-            <label class="label text--flicking">${label}:</label>
-          </div>
-        `;
-      default:
-        return '';
-    }
-  }
 
   Datos(
     nombre: string,
